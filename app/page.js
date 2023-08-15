30c568b8a5e5cf72f053854ad7bf585b5c732e04
Agung Rosyandi <agungrosyandi@gmail.com>
2023-08-15 08:46:56 +0700

    revision nextjs portfolio 15 agustus redeploy 1

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
+'use client';
+
 import AppSections from './components/App';
+import Navbar from './components/Navbar';
 
 // main app --------------------------------------------------------------------------
 
 export default function Home() {
   return (
     <main className="bg-gray-100">
+      <Navbar />
       <AppSections />
     </main>
   );
--- app/showchase/page.js
+++ app/showchase/page.js
@@ -1,13 +1,17 @@
+'use client';
+
 import PosterSections from './Poster';
 import VectorSections from './Vector';
 import MotionSections from './Motion';
 import LandingPageSections from './LandingPage';
+import Navbar from '../components/Navbar';
 
 // main component --------------------------------------------------
 
 export default function Showchase() {
   return (
     <main className="bg-gray-100">
+      <Navbar />
       <VectorSections />
       <PosterSections />
       <MotionSections />
